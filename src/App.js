2862891b62bcff9001c9fabc6c636e2b273919fc
Victor Rueda <vialruma1985@gmail.com>
2022-12-13 13:53:48 -0500

    The component title was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title/Title';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Title/>
     </div>
   );
 }
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../css/Title.css';
+
+
+const Title = () => {
+  return (
+    <div className='title'>
+      <h1>Task App</h1>
+    </div>
+  )
+}
+
+export default Title;
